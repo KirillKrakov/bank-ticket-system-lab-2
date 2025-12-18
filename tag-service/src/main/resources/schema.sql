ad9ecda3556f91fcc4da97f83ebfc287f2d51be1
Kirill Krakov <kirill170251@gmail.com>
2025-12-18 18:35:19 +0300

    feat: add liquibase/flyway migrations

--- tag-service/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
--- Таблица тегов
-CREATE TABLE IF NOT EXISTS tag (
-    id UUID PRIMARY KEY,
-    name VARCHAR(200) NOT NULL UNIQUE
-);
-
--- Индексы
-CREATE INDEX IF NOT EXISTS idx_tag_name ON tag(name);
